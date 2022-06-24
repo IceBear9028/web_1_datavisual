a2739553c494f47bee45b1567edcb80d697ed59e
IceBear9028 <tlatms9028@gmail.com>
2022-06-25 01:18:31 +0900

    2 commit
    1. className 선택자로 넣을때, .클래스이름{}순으로 넣기
    2. div{}로 스타일 넣으면 div태그가 적혀있는 모든 element 에 적용됨

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react';
 
 
 function App() {
 
-  let Title = "여러 데이터들중 하나"
+  let Title = "여러 데이터들중 하나";
+  let [titleChange, THfunction] = useState('안녕하세요');
+    // -> 웹app 처럼 만들때 매우 중요함.
+    //    글 제목, 내용등이 바뀌게 하고 싶을 때, html 재랜더링을 새로 해서 동적인 움직임 제공.
+  const [a,b] = [10,20];
 
+
+  //아래 return 내부는 jsx 문법을 사용한다.
   return (
     <div className="App">
+
       <div className = "headTitle">
-        <h4>Dawin Data</h4> {/*생코드로 작성가능.하지만 변수로 두는것도 가능*/}
+        <h4 style={ {color : "white",fontSize : "20px"} }>Dawin Data</h4> {/*생코드로 작성가능.하지만 변수로 두는것도 가능*/}
+        {/*style 적용시, 중괄호와 안의 속성들도 중괄호(오브젝트형식)로 묶어야 한다.*/}
       </div>
+
       <h2>{ Title }</h2>{/*변수 넣을때는 꼭 대괄호! */}
+      
+      <div className = "List1">
+        <h4>{ titleChange }</h4>
+        <button className = 'button' >이상윤 입니다!</button>
+      </div>
+      
+      <div className = "List1">
+        <h4>안녕하세요</h4>
+        <p>아이폰 입니다!</p>
+      </div>
+    
     </div>
   );
 }
