33d6f55befdcb8b8f26a17b533b192c5a849772e
IceBear9028 <tlatms9028@gmail.com>
2022-06-24 23:17:29 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+
 function App() {
+
+  let Title = "여러 데이터들중 하나"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className = "headTitle">
+        <h4>Dawin Data</h4> {/*생코드로 작성가능.하지만 변수로 두는것도 가능*/}
+      </div>
+      <h2>{ Title }</h2>{/*변수 넣을때는 꼭 대괄호! */}
     </div>
   );
 }
